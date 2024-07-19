ca6dedfa7475df1478591978d7904bbfe2ccaf3d
SahilSharma2911 <sahilsharmarksh@gmail.com>
2024-07-19 11:45:04 +0530

    sadjfbwjbg

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,27 +14,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URL)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
 app.use("/api/pins", pinRoute);
 app.use("/api/users", userRoute);
 
-// Serve static files from the Vite build
-// app.use(express.static(path.join(__dirname, '../../frontend/dist')));
-
 app.get("/api/test", (req, res) => {
   res.json({ message: "hello" });
 });
-
-// Catch all other routes and return the React app
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../../frontend/dist', 'index.html'));
-// });
 
 const PORT = process.env.PORT || 8802;
 app.listen(PORT, () => {
