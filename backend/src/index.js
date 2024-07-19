408b5ae57dfcb44db155fda058bb92445ff9abb8
SahilSharma2911 <sahilsharmarksh@gmail.com>
2024-07-19 09:50:22 +0530

    asdf

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,26 +13,28 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log("MongoDB connected"))
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
 app.use("/api/pins", pinRoute);
 app.use("/api/users", userRoute);
 
 // Serve static files from the Vite build
-app.use(express.static(path.join(__dirname, '../../frontend/dist')));
+// app.use(express.static(path.join(__dirname, '../../frontend/dist')));
 
-app.get('/api/test', (req, res) => {
+app.get("/api/test", (req, res) => {
   res.json({ message: "hello" });
 });
 
 // Catch all other routes and return the React app
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../frontend/dist', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../../frontend/dist', 'index.html'));
+// });
 
 const PORT = process.env.PORT || 8802;
 app.listen(PORT, () => {
